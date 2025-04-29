63fda265f28cdef56dbe4dc66815992a8f66e1a2
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-04-29 10:43:13 +0200

    Create CustomHeaderBasedPartitionInterceptor.java

---
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/CustomHeaderBasedPartitionInterceptor.java
@@ -0,0 +1,47 @@
+package ca.uhn.fhir.jpa.starter.common;
+
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Location;
+import org.hl7.fhir.r4.model.Organization;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Practitioner;
+import org.hl7.fhir.r4.model.PractitionerRole;
+
+import ca.uhn.fhir.interceptor.api.Hook;
+import ca.uhn.fhir.interceptor.api.Interceptor;
+import ca.uhn.fhir.interceptor.api.Pointcut;
+import ca.uhn.fhir.interceptor.model.RequestPartitionId;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+
+@Interceptor
+public class CustomHeaderBasedPartitionInterceptor {
+ 
+	@Hook(Pointcut.STORAGE_PARTITION_IDENTIFY_CREATE)
+	public RequestPartitionId PartitionIdentifyCreate(IBaseResource theResource, RequestDetails theRequestDetails) {
+		return calculatePartition(theResource, theRequestDetails);
+	}
+	
+	@Hook(Pointcut.STORAGE_PARTITION_IDENTIFY_READ)
+	public RequestPartitionId PartitionIdentifyRead(IBaseResource theResource, RequestDetails theRequestDetails) {
+	    return calculatePartition(theResource, theRequestDetails);
+	}
+	
+	private RequestPartitionId calculatePartition(IBaseResource theResource, RequestDetails theRequestDetails) {
+	    String partitionName = theRequestDetails.getHeader("X-Tenant-ID");
+
+	    if (theResource instanceof Patient) {
+	        return RequestPartitionId.fromPartitionName("PATIENT");
+	    } else if (theResource instanceof Location) {
+	        return RequestPartitionId.fromPartitionName("LOCATION");
+	    } else if (theResource instanceof Organization) {
+	        return RequestPartitionId.fromPartitionName("ORGANIZATION");
+	    } else if (theResource instanceof Practitioner) {
+	        return RequestPartitionId.fromPartitionName("PRACTITIONER");
+	    } else if (theResource instanceof PractitionerRole) {
+	        return RequestPartitionId.fromPartitionName("PRACTITIONER_ROLE");
+	    } else {
+	    	 return RequestPartitionId.fromPartitionName(partitionName.toUpperCase()); 
+	    }
+	}
+   
+}
